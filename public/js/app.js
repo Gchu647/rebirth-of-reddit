86a77fce06ce314305244520f0b42dacce4f2fda
gchu <gchu647@gmail.com>
2018-07-16 17:13:40 -1000

    Got preview images up

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,7 @@
 let redObj = {} //store info from reddit
 let reqRed = new XMLHttpRequest();
 reqRed.addEventListener("load", getSubReddit);
-reqRed.open("GET", "https://www.reddit.com/r/comics.json"); //The link can be a changeable variable
+reqRed.open("GET", "https://www.reddit.com/r/comics.json?raw_json=1"); //The link can be a changeable variable
 reqRed.send();
 
 function getSubReddit() {
@@ -18,8 +18,6 @@ function makeCards(cardCount) {
   for (let i = 0; i < cardCount; i++) {
     let cards = document.createElement("div");
     cards.className = "cards";
-    console.log(cardsParent);
-    console.log(cards);
 
     let cardsImg = document.createElement("div");
     cardsImg.className = "cards-img";
@@ -34,20 +32,23 @@ function makeCards(cardCount) {
     cards.appendChild(cardsAuthor);
 
     // Assemble a function to assemble information
-    assembleCardInfo(cardsTitle, cardsAuthor, i);
+    assembleCardInfo(cardsImg, cardsTitle, cardsAuthor, i);
 
     cardsParent.appendChild(cards);
   }
 }
 
 // Puts reddit info into the the cards
-function assembleCardInfo(title, content, count) {
+function assembleCardInfo(cardsImg, title, content, count) {
+  let imgInfo = redObj["data"]["children"][count]["data"]["preview"]["images"][0]["resolutions"][2]["url"];
+  console.log(redObj["data"]["children"][count]["data"]["preview"]["images"][0]["resolutions"]);
   let titleInfo = redObj["data"]["children"][count]["data"]["title"];
   let contentInfo = redObj["data"]["children"][count]["data"]["author"];
 
+  cardsImg.style.backgroundImage = `url(${imgInfo})`;
+
   if(titleInfo.length > 58) { //Keep title length in check
     let shortTitle = titleInfo.split("").slice(0, 58).join("");
-    console.log(shortTitle);
     title.innerHTML = `${shortTitle}...`;
   } else {
     title.innerHTML =  titleInfo;
